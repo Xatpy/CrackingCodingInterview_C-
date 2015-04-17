6562bc8f405e904a7e93ee880ed3c6601205de77
Xatpy <jaime.chapinal@gmail.com>
2015-04-17 21:36:31 +0200

    adding chapter3 structure

---
+++ Chapter3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingCodingInterview
+{
+    class Chapter3
+    {
+    }
+
+    class Stack
+    {
+        Node top;
+
+        Node pop()
+        {
+            if (top != null)
+            {
+                Object item = top.Data;
+                top = top.Next;
+                return (Node)item;
+            }
+            return null;
+        }
+
+        void push(Object item)
+        {
+            Node t = new Node((int)item);
+            t.Next = top;
+            top = t;
+        }
+
+        /*Node min()
+        {
+
+        }*/
+    }
+}
